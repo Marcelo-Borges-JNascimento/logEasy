1069d27e4f7a47b8b1277cb04a75366d639d7628
Marcelo-Borges-JNascimento <m_celo.b@hotmail.com>
2026-05-10 19:35:01 -0300

    Estilização e alerts na template Editar

---
+++ logEasy/static/js/scripts3.js
@@ -0,0 +1,10 @@
+const salvar_bt = document.getElementById('salvar_bt')
+const botao_voltar = document.getElementById('botao_link')
+
+salvar_bt.addEventListener('click', () => {
+    window.alert('Edição concluída!')
+});
+
+botao_voltar.addEventListener('click', () => {
+    window.alert('Edição Cancelada!')
+});
